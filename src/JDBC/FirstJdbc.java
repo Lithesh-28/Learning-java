1ddfadf0eb652312c4d66efa4170572c032e71b5
lithesh-28 <litheshjshetty@gmail.com>
2025-08-08 17:03:26 +0530

    Crreated a jdbc querry and executed

--- src/JDBC/FirstJdbc.java
+++ src/JDBC/FirstJdbc.java
@@ -1,4 +1,23 @@
 package JDBC;
 
+import java.sql.*;
+
 public class FirstJdbc {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String sql;
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lith","root","lithesh@2004");
+
+        Statement smt = con.createStatement();
+
+        sql = "insert into student values(101,'pavan','Karkala','general')";
+        int result = smt.executeUpdate(sql);
+        if (result > 0) {
+            System.out.println("Student details added sucessfully");
+        } else {
+            System.out.println("Student details not added");
+        }
+
+    }
+
 }
